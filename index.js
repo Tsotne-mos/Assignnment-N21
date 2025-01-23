5fc039f5c5ed272fa3bb03ad1d515950a9729d80
Tsotne-mos <mosashvilitsotne@gnail.com>
2025-01-23 05:55:44 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+
+let x = 1, y = 3;
+console.log( y - x );
+
+console.log( 5 % 2 );
+console.log( 8 % 3 );
+console.log( 8 % 4 );
+
+console.log( 2 ** 2 );
+console.log( 2 ** 3 );
+console.log( 2 ** 4 );
+
+let s = "my" + "string";
+console.log(s);
+
+console.log( '1' + 2 );
+console.log( 2 + '1' );
+
+console.log(2 + 2 + '1' );
+
+console.log('1' + 2 + 2)
+
+console.log( 6 - '2' );
+console.log( '6' / '2' );
+
+
+let b = 1;
+console.log( +b );
+
+let d = -2;
+console.log( d );
+
+
+console.log( +true );
+console.log( +"" );
+
+let e, f, g;
+
+e = f = g = 2 + 2;
+
+console.log( e );
+console.log( f );
+console.log( g );
+
+let n = 2;
+
+n *= 3 + 5;
+
+console.log( n );
+
+
+
+let counter = 0;
+counter++;
+++counter;
+console.log( counter );
+
+console.log( 2 > 1 );
+console.log( 2 == 1 );
+console.log( 2 != 1 );
+
+console.log( 2 > 1 );
+console.log( 2 == 1 );
+console.log( 2 != 1 );
+
+let k = 0;
+console.log( Boolean(k) );
+
+let j = "0";
+console.log( Boolean(j) );
+
+console.log(k == j);
+
+console.log( null === undefined );
+console.log( null == undefined );
+
+console.log( null > 0 );
+console.log( null == 0 );
+console.log( null >= 0 );
+
+console.log( undefined > 0 );
+console.log( undefined < 0 );
+console.log( undefined == 0 );
+
